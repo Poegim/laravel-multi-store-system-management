92e612830b4c346b46f2b37fbd31f651877d962e
Poegim <jacekwiniarczyk@gmail.com>
2024-04-28 19:02:52 +0200

    Create button in users crud

--- resources/views/livewire/management/user/show-users.blade.php
+++ resources/views/livewire/management/user/show-users.blade.php
@@ -1,7 +1,16 @@
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="w-full flex justify-end my-4">
+            <x-button >
+                <a href="#" class="flex">
+                    <x-fas-plus class="w-6 h-6 mr-2"/>
+                    <span class="my-auto">
+                        {{ __('CREATE') }}
+                    </span>
+                </a>
+            </x-button>
+        </div>
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-
 
             <div class="relative overflow-x-auto">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -20,16 +29,18 @@
                     </thead>
                     <tbody>
                         @foreach($users as $item)
-                        <tr class="bg-white dark:bg-gray-800">
+                        <tr class="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700">
                             <th scope="row"
-                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {{$item->name}}
                             </th>
-                            <td class="px-6 py-4">
+                            <td class="px-6 py-2">
                                 {{$item->email}}
                             </td>
-                            <td class="px-6 py-4">
-                                <button wire:click="edit({{ $item->id }})">Edit</button>
+                            <td class="px-6 py-2">
+                                <button wire:click="edit({{ $item->id }})" class="text-purple-500 dark:hover:text-purple-300 hover:text-purple-700 transition-colors duration-300">
+                                    <x-fas-edit class="h-6 w-6"/>
+                                </button>
                             </td>
                         </tr>
                         @endforeach
