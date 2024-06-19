3cfbb45cb320a78b2a00a637db1cb06be9f28c9f
Poegim <jacekwiniarczyk@gmail.com>
2024-06-19 18:56:44 +0200

    Sidebar nav changes, todo changes

--- resources/views/components/sidebar-b.blade.php
+++ resources/views/components/sidebar-b.blade.php
@@ -1,5 +1,6 @@
 <div x-data="{ navigation: true }" class="z-40">
-        <button @click="navigation = ! navigation"
+
+<!-- <button @click="navigation = ! navigation" data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar"
         aria-controls="default-sidebar" type="button"
         class="fixed top-2 right-2 z-50 inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg border border-1 border-slate-700 sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-600 dark:bg-gray-200">
         <span class="sr-only">Open sidebar</span>
@@ -9,7 +10,7 @@
                 d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
             </path>
         </svg>
-        </button>
+        </button> -->
 
     <aside :class="navigation ? 'sm:translate-x-0' : ''" id="default-sidebar"
         class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
