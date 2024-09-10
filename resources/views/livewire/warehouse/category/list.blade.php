66aa0eadf64ae9650c742d182ddf112fdb093221
poegim <jacekwiniarczyk@gmail.com>
2024-09-10 17:58:31 +0200

    Show categories design fixes

--- resources/views/livewire/warehouse/category/list.blade.php
+++ resources/views/livewire/warehouse/category/list.blade.php
@@ -16,22 +16,20 @@
                     {{ $category['plural_name'] }}
                 </span>
 
-                <span class="flex xl:justify-end space-x-2">
+                <span class="flex justify-end space-x-2">
                     @if($parent)
-                    <span class="flex text-sm text-gray-400 dark:text-gray-500 italic my-auto">
-                        <span class="flex md:hidden">^</span>
-                        <span class="md:flex hidden">{{ __('parent category') }}</span>
-                        : {{ $parent? $parent['plural_name'] : ''}}
+                    <span class="hidden xl:flex text-sm text-gray-400 dark:text-gray-500 italic my-auto">
+                        {{ __('parent category') }}: {{ $parent? $parent['plural_name'] : ''}}
                     </span>
                     @endif
                     @if ($category['disabled'] == true)
-                    <div class="my-auto min-h-7 max-h-7 flex rounded-mid  px-2 text-white italic text-xs bg-gradient-to-tr from-red-600 to-red-900 ">
+                    <div class="my-auto min-h-5 max-h-5 sm:min-h-7 sm:max-h-7 flex rounded-mid  px-2 roboto tracking-wider text-white italic text-xs bg-gradient-to-tr from-red-600 to-red-900 ">
                         <span class="my-auto mx-auto">
                             disabled
                         </span>
                     </div>
                     @elseif($category['disabled'] == false)
-                    <div class="my-auto min-h-7 max-h-7  flex rounded-mid  px-2 py-1 text-white italic text-xs bg-gradient-to-tr from-green-600 to-green-400 ">
+                    <div class="my-auto min-h-5 max-h-5  sm:min-h-7 sm:max-h-7  flex rounded-mid  px-2 roboto tracking-wider text-black italic text-xs bg-gradient-to-tr from-green-600 to-green-400 ">
                         <span class="my-auto mx-auto">
                             enabled
                         </span>
