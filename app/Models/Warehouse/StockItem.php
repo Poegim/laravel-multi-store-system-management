eee458c31933d52dede31417dc75ad4559729dd7
poegim <jacekwiniarczyk@gmail.com>
2024-11-04 20:16:04 +0100

    Shwo Variant

--- app/Models/Warehouse/StockItem.php
+++ app/Models/Warehouse/StockItem.php
@@ -2,6 +2,9 @@
 
 namespace App\Models\Warehouse;
 
+use App\Models\Store;
+use App\Traits\GetsFormattedAmount;
+use App\Traits\HasFormattedSRP;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,9 +12,26 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class StockItem extends Model
 {
     use HasFactory;
+    use HasFormattedSRP;
+    use GetsFormattedAmount;
+
+    public function formattedPurchasePriceNet()
+    {
+        return $this->getFormattedAmount($this->purchase_price_net);
+    }
+
+    public function formattedPurchasePriceGross()
+    {
+        return $this->getFormattedAmount($this->purchase_price_gross);
+    }
 
     public function brand(): BelongsTo
     {
         return $this->belongsTo(Brand::class);
+    }
+
+    public function store(): BelongsTo
+    {
+        return $this->belongsTo(Store::class);
     }
 }
--- resources/views/warehouse/product_variant/show.blade.php
+++ resources/views/warehouse/product_variant/show.blade.php
@@ -8,11 +8,66 @@
     </x-slot>
     
     <x-window>
-        @foreach ($productVariant->stockItems as $stockItem)
-        <p>
-            {{ $stockItem->id}}, {{$stockItem->color}}, {{ $stockItem->store_id}}
-        </p>    
-        @endforeach
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs uppercase">
+                <tr class="text-black dark:text-white">
+                    <th scope="col" class="px-6 py-3 uppercase">
+                            {{__('id')}}
+                    </th>
+                    <th scope="col" class="px-6 py-3 uppercase">
+                            {{__('brand')}}
+                    </th>
+                    <th scope="col" class="px-6 py-3 uppercase">
+                            {{__('SRP')}}
+                    </th>
+                    <th scope="col" class="px-6 py-3 uppercase">
+                            {{__('PPN')}}
+                    </th>
+                    <th scope="col" class="px-6 py-3 uppercase">
+                            {{__('PPG')}}
+                    </th>
+                    <th scope="col" class="px-6 py-3 uppercase">
+                            {{__('color')}}
+                    </th>
+                    <th scope="col" class="px-6 py-3 uppercase">
+                            {{__('store')}}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($productVariant->stockItems as $stockItem)
+                <tr class="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700" >
+                    <td class="px-6 py-1 dark:text-gray-100 font-thin">
+                        {{ $stockItem->id }}
+                    </td>
+                    <td class="px-6 py-1 dark:text-gray-100">
+                        <a href="{{ route('brand.show', $stockItem->brand->slug) }}" class="link">
+                            {{ $stockItem->brand->name }}
+                        </a>
+                    </td>
+                    <td class="px-6 py-1 dark:text-gray-100 font-thin">
+                        {{ $stockItem->formattedSRP() }}
+                    </td>
+                    <td class="px-6 py-1 dark:text-gray-100 font-thin">
+                        {{ $stockItem->formattedPurchasePriceNet() }}
+                    </td>
+                    <td class="px-6 py-1 dark:text-gray-100 font-thin">
+                        {{ $stockItem->formattedPurchasePriceGross() }}
+                    </td>
+                    <td class="px-6 py-1 dark:text-gray-100 font-thin">
+                        {{ $stockItem->color }}
+                    </td>
+                    <td class="px-6 py-1 dark:text-gray-100">
+                        <a href="{{ route('store.show', $stockItem->store->id )}}" class="link">
+                            {{ $stockItem->store->name }}
+                        </a>
+                    </td>
+                </tr>
+                @endforeach
+                
+            </tbody>
+        </table>
+        
     </x-window>    
 
 </x-app-layout>
