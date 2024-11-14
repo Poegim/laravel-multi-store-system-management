ada6ead4295c54d3e0742481ceb90fd23a5f290f
poegim <jacekwiniarczyk@gmail.com>
2024-11-14 12:04:13 +0100

    Improve: create contact

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -27,11 +27,11 @@
                     <label for="type" class="input-label">{{__('type')}}</label>
                     <div class="flex space-x-2">
                         <span @click="type = 'person'" >
-                            <input class="my-auto" type="radio" id="person" name="type" value="1" />
+                            <input class="my-auto" type="radio" id="person" name="type" value="1" {{ old('type') == 1 ? 'checked' : ''}}/>
                             <label for="person">{{__('person')}}</label>
                         </span>
                         <span  @click="type = 'company'">
-                            <input class="my-auto" type="radio" id="company" name="type" value="2" />
+                            <input class="my-auto" type="radio" id="company" name="type" value="2" {{ old('type') == 2 ? 'checked' : ''}} />
                             <label for="company">{{__('company')}}</label>
                         </span>
                     </div>
@@ -40,78 +40,92 @@
 
                 <div>
                     <label for="name" class="input-label">{{__('name')}}</label>
-                    <x-input type="text" name="name" id="name" class="w-full" required />
+                    <x-input type="text" name="name" id="name" class="w-full" required
+                    value="{{ old('name') ? old('name') : ''}}"
+                    />
                 </div>
 
                 <div>
                     <div :class="type == 'person' ? '' : 'hidden'">
                         <label for="surname" class="input-label">{{__('surname')}}</label>
-                        <x-input type="text" name="surname" id="surname" class="w-full" />
+                        <x-input type="text" name="surname" id="surname" class="w-full"
+                        value="{{ old('surname') ? old('surname') : ''}}"/>
                     </div>
                 </div>
 
                 <div class="md:col-span-2">
                     <label for="identification_number" class="input-label">{{__('identification_number')}}</label>
-                    <x-input type="text" name="identification_number" id="identification_number" class="w-full md:w-1/2" />
+                    <x-input type="text" name="identification_number" id="identification_number" class="w-full md:w-1/2"
+                    value="{{ old('identification_number') ? old('identification_number') : ''}}" />
                 </div>
 
                 <div>
-                    <label for="contry" class="input-label">{{__('contry')}}</label>
-                    <x-input type="text" name="contry" id="contry" class="w-full" value="Polska" />
+                    <label for="country" class="input-label">{{__('country')}}</label>
+                    <x-input type="text" name="country" id="country" class="w-full" value="Polska"
+                    value="{{ old('country') ? old('country') : ''}}"/>
                 </div>
 
                 <div>
                     <label for="city" class="input-label">{{__('city')}}</label>
-                    <x-input type="text" name="city" id="city" class="w-full" />
+                    <x-input type="text" name="city" id="city" class="w-full"
+                    value="{{ old('city') ? old('city') : ''}}"/>
                 </div>
                 <div>
                     <label for="postcode" class="input-label">{{__('postcode')}}</label>
-                    <x-input type="text" name="postcode" id="postcode" class="w-full" />
+                    <x-input type="text" name="postcode" id="postcode" class="w-full"
+                    value="{{ old('postcode') ? old('postcode') : ''}}"/>
                 </div>
 
                 <div>
                     <label for="street" class="input-label">{{__('street')}}</label>
-                    <x-input type="text" name="street" id="street" class="w-full" />
+                    <x-input type="text" name="street" id="street" class="w-full"
+                    value="{{ old('street') ? old('street') : ''}}"/>
                 </div>
 
                 <div>
                     <label for="building_number" class="input-label">{{__('building_number')}}</label>
-                    <x-input type="text" name="building_number" id="building_number" class="w-full" />
+                    <x-input type="text" name="building_number" id="building_number" class="w-full"
+                    value="{{ old('building_number') ? old('building_number') : ''}}"/>
                 </div>
 
                 <div>
                     <label for="apartment_number" class="input-label">{{__('apartment_number')}}</label>
-                    <x-input type="text" name="apartment_number" id="apartment_number" class="w-full" />
+                    <x-input type="text" name="apartment_number" id="apartment_number" class="w-full"
+                    value="{{ old('apartment_number') ? old('apartment_number') : ''}}"/>
                 </div>
 
                 <div>
                     <label for="email" class="input-label">{{__('email')}}</label>
-                    <x-input type="email" name="email" id="email" class="w-full" />
+                    <x-input type="email" name="email" id="email" class="w-full"
+                    value="{{ old('email') ? old('email') : ''}}"/>
                 </div>
 
                 <div>
                     <label for="phone" class="input-label">{{__('phone')}}</label>
-                    <x-input type="text" name="phone" id="phone" class="w-full" />
+                    <x-input type="text" name="phone" id="phone" class="w-full"
+                    value="{{ old('phone') ? old('phone') : ''}}"/>
                 </div>
 
                 <div>
                     <label for="second_phone" class="input-label">{{__('second_phone')}}</label>
-                    <x-input type="text" name="second_phone" id="second_phone" class="w-full" />
+                    <x-input type="text" name="second_phone" id="second_phone" class="w-full"
+                    value="{{ old('second_phone') ? old('second_phone') : ''}}"/>
                 </div>
 
                 <div>
                     <label for="www" class="input-label">{{__('www')}}</label>
-                   <x-input type="text" name="www" id="www" class="w-full" />
-                </div>    
+                   <x-input type="text" name="www" id="www" class="w-full"
+                   value="{{ old('www') ? old('www') : ''}}"/>
+                </div>
 
                 <div class="md:col-span-2">
                     <label for="description" class="input-label">{{__('description')}}</label>
-                    <textarea class="rounded w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900" type="text" name="description" id="description" class="w-full" > </textarea>
+                    <textarea class="rounded w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900"
+                              type="text" name="description" id="description">
+                        {{ old('description', '') }}
+                    </textarea>
                 </div>
-
-
             </div>
-
 
             <x-submit-cancel-btns :cancelRoute="'contact.index'" :type="'create'" />
 
