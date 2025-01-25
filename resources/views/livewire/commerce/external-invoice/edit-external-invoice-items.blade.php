a9c771d1f666557d83ea270c7f7bbd4fc602b66b
poegim <jacekwiniarczyk@gmail.com>
2025-01-25 17:26:35 +0100

    Add: In Extrenal invoice edit, color picker color indicator

--- resources/views/livewire/commerce/external-invoice/edit-external-invoice-items.blade.php
+++ resources/views/livewire/commerce/external-invoice/edit-external-invoice-items.blade.php
@@ -60,7 +60,7 @@
                     <div class="overflow-hidden">
                         <div class="flex">
 
-                            <!-- Input wyszukiwania -->
+                            <!-- Input search -->
                             <input type="text" id="search_product_input"
                                 x-model="searchProduct"
                                 wire:model.debounce.500ms.live="searchProduct"
@@ -69,7 +69,7 @@
                             />
 
 
-                            <!-- Przycisk rozwijania -->
+                            <!-- Dropdown button -->
                             <button @click="toggle" id="btn_product"
                                 class="bg-white w-10 border-l-0 rounded-tr-lg rounded-br-lg border border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mx-auto w-4 h-4">
@@ -84,7 +84,7 @@
 
                     </div>
 
-                    <!-- Lista rozwijana -->
+                    <!-- Dropdown list -->
                     <ul
                         x-show="visibleProductsList"
                         x-transition
@@ -162,7 +162,7 @@
                 >
                     <div class="overflow-hidden">
                         <div class="flex">
-                            <!-- Input wyszukiwania -->
+                            <!-- Search Input -->
                             <input type="text" id="search_device_input"
                                 x-model="searchDevice"
                                 wire:model.debounce.500ms.live="searchDevice"
@@ -171,7 +171,7 @@
                             />
 
 
-                            <!-- Przycisk rozwijania -->
+                            <!-- Dropdown button -->
                             <button @click="toggle" id="btn_device"
                                 class="bg-white w-10 border-l-0 rounded-tr-lg rounded-br-lg border border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mx-auto w-4 h-4">
@@ -186,7 +186,7 @@
 
                     </div>
 
-                    <!-- Lista rozwijana -->
+                    <!-- Dropdown list -->
                     <ul
                         x-show="visibleDevicesList"
                         x-transition
@@ -213,21 +213,16 @@
 
             <div x-data="{ expanded: false }" @click.outside="expanded = false">
                 <!-- COLOR -->
-                <div class="space-y-2">
+                <div class="">
                     <div class="relative">
-
                         <x-label for="color">
                                 {{ __('color') }}:
                                 {{ $color ? $color["name"] : '' }}
                         </x-label>
-
-
                     </div>
 
                     <div class="flex relative" x-data="{ expanded: false }" >
-
                         <div class="flex w-full ">
-
                             <input
                             @click="expanded = true"
                             x-model="selectedColor"
@@ -243,6 +238,24 @@
                                     </svg>
                                 </span>
                             </button>
+                            
+                            <div 
+                                class="my-auto ml-1 p-1"  
+                                style="
+                                    height: 4px;
+                                    width: 4px;
+                                    border-radius: 50%;
+                                    display: inline-block;
+                                    background-color: {{ $color ? $color['value'] : 'transparent' }};
+                                "
+                            >
+                            </div>
+                            
+                            <div 
+                                class="mx-1 flex text-blue-700 dark:text-green-300 w-[20px] rounded-full" 
+                            >
+                                <svg wire:loading wire:target="setColor" fill="currentColor" class="animate-spin my-auto size-6" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path class="fil0" d="M854.569 841.338c-188.268 189.444 -519.825 171.223 -704.157 -13.109 -190.56 -190.56 -200.048 -493.728 -28.483 -695.516 10.739 -12.623 21.132 -25.234 34.585 -33.667 36.553 -22.89 85.347 -18.445 117.138 13.347 30.228 30.228 35.737 75.83 16.531 111.665 -4.893 9.117 -9.221 14.693 -16.299 22.289 -140.375 150.709 -144.886 378.867 -7.747 516.005 152.583 152.584 406.604 120.623 541.406 -34.133 106.781 -122.634 142.717 -297.392 77.857 -451.04 -83.615 -198.07 -305.207 -291.19 -510.476 -222.476l-.226 -.226c235.803 -82.501 492.218 23.489 588.42 251.384 70.374 166.699 36.667 355.204 -71.697 493.53 -11.48 14.653 -23.724 28.744 -36.852 41.948z"></path></g></svg>
+                            </div>
                         </div>
 
                         <div class="h-10">
@@ -267,7 +280,6 @@
                                 </ul>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
