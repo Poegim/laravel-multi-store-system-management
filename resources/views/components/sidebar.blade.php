559bb22163814e556279e2551067d558f7a1d71e
poegim <jacekwiniarczyk@gmail.com>
2025-03-04 13:27:08 +0100

    Sync

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -292,11 +292,9 @@
 
                 <a href="{{ route('profile.show') }}" wire:navigate
                     class="inline-flex justify-center p-2 text-gray-800 dark:text-gray-200 rounded cursor-pointer dark:hover:text-white hover:text-gray-700 dark:hover:bg-gray-600 hover:bg-gray-100">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    </svg>
+
+                    <img src="{{ asset(auth()->user()->profile_photo_url) }}" alt="{{ auth()->user()->name }}"
+                                class="rounded-full w-12 h-12 md:h-8 md:w-8 object-cover mr-2">
                 </a>
 
                 {{-- <a href="#"
@@ -310,7 +308,7 @@
                 </a> --}}
 
 
-                <div class="mt-2" data-tooltip-target="tooltip-mode">
+                <div class="my-auto" data-tooltip-target="tooltip-mode">
                     <button type="button" x-bind:class="darkMode ? 'bg-sky-200' : 'bg-sky-900'"
                         x-on:click="darkMode = !darkMode"
                         class="relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-2 hover:bg-sky-500"
