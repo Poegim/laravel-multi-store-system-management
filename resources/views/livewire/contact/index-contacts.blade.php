463799c53a4a13df43eed0b0b4b9ac2e059a67be
poegim <jacekwiniarczyk@gmail.com>
2025-06-12 09:44:35 +0200

    Contacts index view refactor

--- resources/views/livewire/contact/index-contacts.blade.php
+++ resources/views/livewire/contact/index-contacts.blade.php
@@ -10,12 +10,11 @@
     </div>
 
     <x-window>
-
-        <div>
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs uppercase">
-                    <tr class="text-black dark:text-white">
-                        <th scope="col" class="px-6 py-3">
+        <div class="overflow-x-auto">
+            <table class="rounded-2xl overflow-hidden min-w-full text-xs text-left text-gray-700 dark:text-gray-300 border dark:border-gray-700">
+                <thead class="uppercase bg-gray-50 dark:bg-gray-700 text-gray-600 dark:text-gray-300">
+                    <tr>
+                        <th scope="col" class="px-4 py-2">
                             <div class="flex cursor-pointer" wire:click="sortBy('id')">
                                 <span class="uppercase">
                                     {{__('id')}}
@@ -28,7 +27,7 @@
                                 </svg>
                             </div>
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="px-4 py-2">
                             <div class="flex cursor-pointer" wire:click="sortBy('name')">
                                 <span class="uppercase">
                                     {{__('name')}}
@@ -41,71 +40,71 @@
                                 </svg>
                             </div>
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="px-4 py-2">
                             <div class="flex cursor-pointer">
                                 <span class="uppercase">
-                                    {{__('identification_number')}}
+                                    {{__('identification number')}}
                                 </span>
                             </div>
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="px-4 py-2 hidden lg:table-cell">
                             <div class="flex cursor-pointer">
                                 <span class="uppercase">
                                     {{__('type')}}
                                 </span>
                             </div>
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="px-4 py-2 hidden lg:table-cell">
                             <div class="flex cursor-pointer">
                                 <span class="uppercase">
                                     {{__('email')}}
                                 </span>
                             </div>
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="px-4 py-2 hidden lg:table-cell">
                             <div class="flex cursor-pointer">
                                 <span class="uppercase">
                                     {{__('phone')}}
                                 </span>
                             </div>
                         </th>
-                        <th scope="col" class="px-6 py-3 text-right">
+                        <th scope="col" class="px-4 py-2 text-right">
 
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach($contacts as $contact)
-                    <tr class="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700"
+                    <tr class="bg-white dark:bg-gray-800 border-t hover:bg-gray-100 dark:hover:bg-gray-700 transition"
                         wire:key="row-{{ $contact->id }}">
-                        <td class="px-6 py-1 dark:text-gray-100 font-thin">
+                        <td class="px-4 py-2 dark:text-gray-100 font-thin">
                             {{$contact->id}}
                         </td>
 
-                        <td scope="row" class="px-6 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <td scope="row" class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             <div class="flex">
                                 <a href="{{ route('contact.show', $contact) }}" class="link my-auto" alt="{{$contact->name}}"
                                     label="{{$contact->name}}">{{Illuminate\Support\Str::limit($contact->name, 30, '...')}}</a>
                             </div>
                         </td>
 
-                        <td scope="row" class="px-6 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <td scope="row" class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {{ $contact->identification_number}}
                         </td>
 
-                        <td class="px-6 py-1 hidden lg:table-cell">
+                        <td class="px-4 py-2 hidden lg:table-cell">
                             {{$contact->type()}}
                         </td>
 
-                        <td class="px-6 py-1 hidden lg:table-cell">
+                        <td class="px-4 py-2 hidden lg:table-cell">
                             {{$contact->email}}
                         </td>
 
-                        <td class="px-6 py-1 hidden lg:table-cell">
+                        <td class="px-4 py-2 hidden lg:table-cell">
                             {{$contact->phone}}
                         </td>
 
-                        <td class="px-6 py-1 flex justify-end">
+                        <td class="px-4 py-2 flex justify-end">
                             <a href="{{route('contact.edit', $contact)}}" wire:navigate>
                                 <x-buttons.edit-button >
                                     Edit
@@ -119,7 +118,7 @@
         </div>
 
         <div class=" m-4">
-            {{ $contacts->links() }}
+            {{ $contacts->links(data: ['scrollTo' => false]) }}
         </div>
     </x-window>
 
