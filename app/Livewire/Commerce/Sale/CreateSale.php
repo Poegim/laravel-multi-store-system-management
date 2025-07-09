e9ab23266e7da3745e1ae621f5065f84e49e07c0
poegim <jacekwiniarczyk@gmail.com>
2025-07-09 20:17:28 +0200

    sync

--- app/Livewire/Commerce/Sale/CreateSale.php
+++ app/Livewire/Commerce/Sale/CreateSale.php
@@ -13,22 +13,37 @@ class CreateSale extends Component
     public ?Sale $sale;
     public $searchItem = '';
 
+
     public function mount(Store $store, ?Sale $sale = null)
     {
         $this->store = $store;
-        $this->sale = $sale;
+        $sale ? $this->sale = $sale : $this->sale = new Sale();
+        if($this->sale->store_id) {
+            if($this->sale->store_id != $this->store->id) {
+                abort(403, 'Unauthorized action, this sale does not belong to the selected store.');
+            }
+        } else {
+            $this->sale->store_id = $this->store->id;
+        }
     }
 
     public function addItem()
     {
+        $this->validate([
+            'searchItem' => 'required|numeric|exists:stock_items,id',
+        ]);
+
         $item = StockItem::available()
             ->where('store_id', $this->store->id)
             ->where('id', $this->searchItem)
             ->first();
 
         if ($item) {
+            $this->resetErrorBag();
+            dd($item);
             $this->sale->items()->attach($item->id);
         } else {
+
             $this->searchItem = '';
             $this->addError('searchItem', 'Item not found!');
         }
